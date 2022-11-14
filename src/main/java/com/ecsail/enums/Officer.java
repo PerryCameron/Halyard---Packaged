67ffa821db8d44933f01b30d26f57a2e6e66d25b
PerryCameron <pcameron28@yahoo.com>
2022-11-13 20:38:44 -0500

    Added docs to Officer class

--- src/main/java/com/ecsail/enums/Officer.java
+++ src/main/java/com/ecsail/enums/Officer.java
@@ -5,16 +5,29 @@ import com.ecsail.sql.select.SqlBoardPositions;
 import com.ecsail.structures.BoardPositionDTO;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-// this used to be an ENUM but changed it to a class and pull info from
-// the database and kept it acting like an enum.
+/**
+ * this used to be an ENUM but changed it to a class and pull info from
+ * the database and kept it acting somewhat like an enum. This gives the
+ * versatility to add, delete, and edit positions, and also allowed for
+ * clearer code by enabling more columns of information
+ */
+
 public class Officer {
-
+    /**
+     * This is the most appropriate location to create an Array list of board positions
+     * @return ArrayList<BoardPositionDTO>
+     */
     public static ArrayList<BoardPositionDTO> getPositionList() {
         return SqlBoardPositions.getPositions();
     }
 
+    /**
+     * Will return the full name of the position, when give a two character code.
+     * This feature is for reducing database storage size needed
+     * @param officerCode - two character code
+     * @return String - full name of position
+     */
     public static String getByCode(String officerCode) {
         return BaseApplication.boardPositions
                 .stream()
@@ -22,12 +35,16 @@ public class Officer {
                 .map(p -> p.position()).findFirst().orElse("not found");
     }
 
+    /**
+     * Well return a two character code when given the full name of a position
+     * @param name String the full name of a position
+     * @return String the two character code that represents given position name
+     */
     public static String getByName(String name) {
         return BaseApplication.boardPositions
                 .stream()
                 .filter(p -> p.position().equals(name))
                 .map(p -> p.identifier()).findFirst().orElse("not found");
     }
-
 
 }
