cfbbc7fc2477efef9fc4c018a9c82d4d127f5f11
PerryCameron <pcameron28@yahoo.com>
2023-02-18 11:38:15 -0500

    Added copy feature for phone number

--- src/main/java/com/ecsail/gui/tabs/membership/people/person/HBoxPhone.java
+++ src/main/java/com/ecsail/gui/tabs/membership/people/person/HBoxPhone.java
@@ -8,6 +8,7 @@ import com.ecsail.sql.SqlInsert;
 import com.ecsail.sql.SqlUpdate;
 import com.ecsail.sql.select.SqlPhone;
 import com.ecsail.sql.select.SqlSelect;
+import com.ecsail.structures.EmailDTO;
 import com.ecsail.structures.PersonDTO;
 import com.ecsail.structures.PhoneDTO;
 import javafx.beans.Observable;
@@ -23,6 +24,8 @@ import javafx.scene.control.*;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.ComboBoxTableCell;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -38,50 +41,44 @@ public class HBoxPhone extends HBox {
     private final PersonDTO person;
     private final TableView<PhoneDTO> phoneTableView;
     private final ObservableList<PhoneDTO> phone;
+    private TableColumn<PhoneDTO, String> Col1;
     
     public HBoxPhone(PersonDTO p) {
         this.person = p;  // the below callback is to allow commit when focus removed, overrides FX default behavior
         this.phone = FXCollections.observableArrayList(param -> new Observable[] { param.isListedProperty() });
         this.phone.addAll(SqlPhone.getPhoneByPid(person.getP_id()));
-        
-        /////// OBJECT INSTANCE //////  
-        VBox vboxButtons = new VBox(); // holds phone buttons
-        Button phoneAdd = new Button("Add");
-        Button phoneDelete = new Button("Delete");
-        HBox hboxGrey = new HBox(); // this is here for the grey background to make nice appearance
-        VBox vboxPink = new VBox(); // this creates a pink border around the table
-        phoneTableView = new TableView<>();
-
-        //// OBJECT ATTRIBUTES /////
-        phoneAdd.setPrefWidth(60);
-        phoneDelete.setPrefWidth(60);
-        vboxButtons.setPrefWidth(80);
-        vboxButtons.setSpacing(5); // spacing between buttons
-        //hboxGrey.setPrefWidth(480);
-        
-        HBox.setHgrow(hboxGrey, Priority.ALWAYS);
-        HBox.setHgrow(vboxPink, Priority.ALWAYS);
-        HBox.setHgrow(phoneTableView, Priority.ALWAYS);
-        VBox.setVgrow(phoneTableView, Priority.ALWAYS);
-        
-        hboxGrey.setSpacing(10);  // spacing in between table and buttons
+        this.phoneTableView = createTableView();
+        VBox vboxButtons = createButtonBox(); // holds phone buttons
+        HBox hboxGrey = createOuterBox(vboxButtons); // this is here for the grey background to make nice appearance
         this.setId("custom-tap-pane-frame");
-        hboxGrey.setId("box-background-light");
-//        vboxPink.setId("box-pink");
+        getChildren().add(hboxGrey);
+    }  // end of constructor
 
-        hboxGrey.setPadding(new Insets(5,5,5,5));  // spacing around table and buttons
+    private HBox createOuterBox(VBox vboxButtons) {
+        HBox hBox = new HBox();
+        VBox vboxPink = new VBox(); // this creates a pink border around the table
+        HBox.setHgrow(vboxPink, Priority.ALWAYS);
+        HBox.setHgrow(hBox, Priority.ALWAYS);
+        hBox.setSpacing(10);  // spacing in between table and buttons
+        hBox.setId("box-background-light");
+        hBox.setPadding(new Insets(5,5,5,5));  // spacing around table and buttons
         vboxPink.setPadding(new Insets(2,2,2,2)); // spacing to make pink frame around table
-        VBox.setVgrow(phoneTableView, Priority.ALWAYS);
+        vboxPink.getChildren().add(phoneTableView);  // adds pink border around table
+        hBox.getChildren().addAll(vboxPink,vboxButtons);
+        return hBox;
+    }
 
-        ///// TABLEVIEW INSTANCE CREATION AND ATTRIBUTES /////
-        
-        phoneTableView.setItems(phone);
-        phoneTableView.setFixedCellSize(30);
-        phoneTableView.setEditable(true);
-        phoneTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY );
-            
+    private TableView<PhoneDTO> createTableView() {
+        TableView<PhoneDTO> tableView = new TableView<>();
+        VBox.setVgrow(tableView, Priority.ALWAYS);
+        HBox.setHgrow(tableView, Priority.ALWAYS);
+        tableView.setItems(phone);
+        tableView.setFixedCellSize(30);
+        tableView.setEditable(true);
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY );
+
         // example for this column found at https://gist.github.com/james-d/be5bbd6255a4640a5357#file-editcell-java-L109
-        TableColumn<PhoneDTO, String> Col1 = createColumn(PhoneDTO::phoneNumberProperty);
+        Col1 = createColumn(PhoneDTO::phoneNumberProperty);
         Col1.setOnEditCommit(
                 new EventHandler<>() {
                     @Override
@@ -130,8 +127,8 @@ public class HBoxPhone extends HBox {
                         return newValue.replaceAll("[^0-9]", "");
                     }
                 }
-            );
-        
+        );
+
         // example for this column found at https://o7planning.org/en/11079/javafx-tableview-tutorial
         ObservableList<PhoneType> phoneTypeList = FXCollections.observableArrayList(PhoneType.values());
         TableColumn<PhoneDTO, PhoneType> Col2 = new TableColumn<>("Type");
@@ -141,9 +138,9 @@ public class HBoxPhone extends HBox {
             PhoneType phoneType = PhoneType.getByCode(phoneCode);
             return new SimpleObjectProperty<>(phoneType);
         });
- 
+
         Col2.setCellFactory(ComboBoxTableCell.forTableColumn(phoneTypeList));
- 
+
         Col2.setOnEditCommit((CellEditEvent<PhoneDTO, PhoneType> event) -> {
             TablePosition<PhoneDTO, PhoneType> pos = event.getTablePosition();
             PhoneType newPhoneType = event.getNewValue();
@@ -152,7 +149,7 @@ public class HBoxPhone extends HBox {
             SqlUpdate.updatePhone("phone_type", thisPhone.getPhone_ID(), newPhoneType.getCode());
             thisPhone.setPhoneType(newPhoneType.getCode());
         });
-        
+
         // example for this column found at https://o7planning.org/en/11079/javafx-tableview-tutorial
         TableColumn<PhoneDTO, Boolean> Col3 = new TableColumn<>("Listed");
         Col3.setCellValueFactory(param -> {
@@ -174,34 +171,59 @@ public class HBoxPhone extends HBox {
             cell.setAlignment(Pos.CENTER);
             return cell;
         });
-        
+
         /// sets width of columns by percentage
         Col1.setMaxWidth( 1f * Integer.MAX_VALUE * 50);   // Phone
         Col2.setMaxWidth( 1f * Integer.MAX_VALUE * 30 );  // Type
         Col3.setMaxWidth( 1f * Integer.MAX_VALUE * 20 );  // Listed
+        tableView.getColumns().addAll(Arrays.asList(Col1,Col2,Col3));
+        return tableView;
+    }
 
-        
-        /////////////////// LISTENERS //////////////////////////////
+    private VBox createButtonBox() {
+        VBox vBox = new VBox();
+        vBox.setPrefWidth(80);
+        vBox.setSpacing(5); // spacing between buttons
+        Button phoneAdd = new Button("Add");
+        Button phoneDelete = new Button("Delete");
+        Button phoneCopy = new Button("Copy");
+        phoneAdd.setPrefWidth(60);
+        phoneDelete.setPrefWidth(60);
+        phoneCopy.setPrefWidth(60);
+        setAddButtonListener(phoneAdd);
+        SetDeleteButtonListener(phoneDelete);
+        setCopyButtonListener(phoneCopy);
+        vBox.getChildren().addAll(phoneAdd, phoneDelete, phoneCopy); // lines buttons up vertically
+        return vBox;
+    }
 
-        phoneAdd.setOnAction((event) -> {
-            BaseApplication.logger.info("Added new phone entry for " + person.getNameWithInfo());
-            // return next key id for phone table
-            int phone_id = SqlSelect.getNextAvailablePrimaryKey("phone", "phone_id");
-            // attempt to add a new record and return if it is successful
-            if (SqlInsert.addPhoneRecord(phone_id, person.getP_id(), true, "new phone", ""))
-                // if successfully added to SQL then add a new row in the tableview
-                phone.add(new PhoneDTO(phone_id, person.getP_id(), true, "", ""));
-            // Now we will sort it to the top
-            phone.sort(Comparator.comparing(PhoneDTO::getPhone_ID).reversed());
-            // this line prevents strange buggy behaviour
-            phoneTableView.layout();
-            phoneTableView.requestFocus();
-            phoneTableView.getSelectionModel().select(0);
-            phoneTableView.getFocusModel().focus(0);
-            // edit the phone number cell after creating
-            phoneTableView.edit(0, Col1);
+    private void setCopyButtonListener(Button phoneCopy) {
+        phoneCopy.setOnAction((copy) -> {
+            int selectedIndex = phoneTableView.getSelectionModel().getSelectedIndex();
+            Clipboard clipboard = Clipboard.getSystemClipboard();
+            final ClipboardContent content = new ClipboardContent();
+            if (selectedIndex >= 0) {// make sure something is selected
+                PhoneDTO phoneDTO = phone.get(selectedIndex);
+                content.putString(phoneDTO.getPhoneNumber());
+                clipboard.setContent(content);
+            } else
+                alertToSelectRow();
         });
+    }
 
+    private void alertToSelectRow() {
+        Alert information = new Alert(Alert.AlertType.INFORMATION);
+        information.setTitle("Row not selected");
+        information.setHeaderText("Opps!");
+        information.setContentText("You must select a row first!");
+        DialogPane dialogPane = information.getDialogPane();
+        dialogPane.getStylesheets().add("css/dark/dialogue.css");
+        dialogPane.getStyleClass().add("dialog");
+        Optional<ButtonType> result = information.showAndWait();
+        result.isPresent();
+    }
+
+    private void SetDeleteButtonListener(Button phoneDelete) {
         phoneDelete.setOnAction((event) -> {
             int selectedIndex = phoneTableView.getSelectionModel().getSelectedIndex();
             if (selectedIndex >= 0) { // make sure something is selected
@@ -221,19 +243,32 @@ public class HBoxPhone extends HBox {
                             + " phone number " + ph.getPhoneNumber()
                             + " from " + person.getNameWithInfo());
                 }
-            }
+            } else
+                alertToSelectRow();
         });
-        
-        ///////////////////  SET CONTENT  ///////////////////////
-        
-        phoneTableView.getColumns().addAll(Arrays.asList(Col1,Col2,Col3));
-        vboxPink.getChildren().add(phoneTableView);  // adds pink border around table
-        vboxButtons.getChildren().addAll(phoneAdd, phoneDelete); // lines buttons up vertically
-        hboxGrey.getChildren().addAll(vboxPink,vboxButtons);
-        getChildren().add(hboxGrey);
-        
-    }  // end of constructor
-    
+    }
+
+    private void setAddButtonListener(Button phoneAdd) {
+        phoneAdd.setOnAction((event) -> {
+            BaseApplication.logger.info("Added new phone entry for " + person.getNameWithInfo());
+            // return next key id for phone table
+            int phone_id = SqlSelect.getNextAvailablePrimaryKey("phone", "phone_id");
+            // attempt to add a new record and return if it is successful
+            if (SqlInsert.addPhoneRecord(phone_id, person.getP_id(), true, "new phone", ""))
+                // if successfully added to SQL then add a new row in the tableview
+                phone.add(new PhoneDTO(phone_id, person.getP_id(), true, "", ""));
+            // Now we will sort it to the top
+            phone.sort(Comparator.comparing(PhoneDTO::getPhone_ID).reversed());
+            // this line prevents strange buggy behaviour
+            phoneTableView.layout();
+            phoneTableView.requestFocus();
+            phoneTableView.getSelectionModel().select(0);
+            phoneTableView.getFocusModel().focus(0);
+            // edit the phone number cell after creating
+            phoneTableView.edit(0, Col1);
+        });
+    }
+
     ////////////////////////  CLASS METHODS //////////////////////////
     private <T> TableColumn<T, String> createColumn(Function<T, StringProperty> property) {
         TableColumn<T, String> col = new TableColumn<>("Phone");
